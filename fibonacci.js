6f363d1f39e5f09e361fc99c75f466f5be2feeb7
GarryWest <garrywest@msn.com>
2024-11-05 09:43:50 -0600

    Added some comments

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,5 @@
+// example of recursion
+// the initial exit rule is important to avoid an infinite loop
 const fibo = (n => {
     if (n === 1 || n === 2){
         return n-1;
@@ -5,6 +7,7 @@ const fibo = (n => {
     return fibo(n-1) + fibo(n-2);
 })
 
+// print first 10 Fibonacci numbers
 for (let x = 1; x <= 10; x ++){
     console.log(fibo(x));
 }
